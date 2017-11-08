461eeabbb2370ca2d18a86c308f2210fd3543c13
Allen Schwartz <allen@Jacobs-MacBook-Air.local>
2017-11-08 14:50:44 -0500

    setup fetch expenses from fb

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -35,3 +35,27 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 });
+
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        
+        return database.ref('expenses')
+        .once('value')
+        .then((snapshot) => {
+            const expenses = [];
+    
+            snapshot.forEach((exp) => {
+                expenses.push({
+                    id: exp.key,
+                    ...exp.val()
+                });
+            });
+            dispatch(setExpenses(expenses));            
+        });
+    };      
+};
